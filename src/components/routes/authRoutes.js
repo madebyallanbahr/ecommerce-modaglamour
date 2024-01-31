7522af9ca03a6e62105e93fdfb07bbc484006d1b
Made by Allan Bahr <madebyallanbahr@gmail.com>
2024-01-31 00:57:35 -0300

    feat: added simple authentication and finished the authentication page with new stylesheet

--- src/components/routes/authRoutes.js
+++ src/components/routes/authRoutes.js
@@ -1,9 +1,34 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
-
+const dotenv = require("dotenv");
+dotenv.configDotenv();
 router.get("/", (req, res) => {
   res.sendFile(path.resolve(path.dirname(__dirname) + "/views/auth.html"));
 });
+router.post("/", (req, res) => {
+  let credentials = {
+    email: req.body.inpmail,
+    pass: req.body.inppass,
+  };
+  authenticateUser(credentials).then((code) => {
+    if (code == 200) {
+      res.redirect("/shop");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+const authenticateUser = async (credentials) => {
+  if (
+    credentials.email == process.env.LOGIN_EMAIL &&
+    credentials.pass == process.env.LOGIN_PASS
+  ) {
+    return 200;
+  } else {
+    return 404;
+  }
+};
 
 module.exports = router;
